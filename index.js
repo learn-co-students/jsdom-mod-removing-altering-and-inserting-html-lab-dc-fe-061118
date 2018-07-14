ad43538f23863c93256c4ef594b2364c5cdfc4d0
Evan Solomon <evanlsolomon@gmail.com>
2018-07-14 21:51:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
+var main = document.getElementById('main');
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+var newHeader = document.createElement('H1');
+newHeader.innerText = ('Evan is the champion!');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
