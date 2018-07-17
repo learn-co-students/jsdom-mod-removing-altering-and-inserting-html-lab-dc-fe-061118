e3d7222bccf83e57b5e8100e4c04c80698496e2e
Ki Ki  Bush <donyabush@gmail.com>
2018-07-17 03:09:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+
+document.body.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.innerText = 'Donya is the champion'
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
